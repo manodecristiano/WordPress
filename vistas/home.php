869704faebf1ffe5c11f4372f81846110b501d01
Cristian <Cristian.carre.online@gmail.com>
2024-03-09 21:54:13 +0100

    test commit

--- vistas/home.php
+++ vistas/home.php
@@ -1,4 +1,4 @@
 <div class="container is-fluid">
-	<h1 class="title">Home</h1>
+	<h1 class="title">Home WORDPRESS</h1>
 	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
 </div>
